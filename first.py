f3f093307510edbc82d87505a859bc3a14781c7b
Арина Миронова <mironova.as@phystech.edu>
2023-03-01 11:49:48 +0300

    download script

---
+++ first.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+
+
+GPIO.output(23, 1)   
+time.sleep(1)
+GPIO.output(23, 0)
+time.sleep(1)
+GPIO.output(23, 1)   
+time.sleep(1)
+GPIO.output(23, 0)
+time.sleep(1)
+GPIO.output(23, 1)   
+time.sleep(1)
+GPIO.output(23, 0)
+time.sleep(1)
+GPIO.output(23, 1)   
+time.sleep(1)
+GPIO.output(23, 0)
+
+
+
+    
+
+
+
